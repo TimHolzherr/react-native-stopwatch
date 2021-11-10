ebde302e335583b533e2bd735f76675744cc815a
Tim Holzherr <tim.holzherr@swisslife.ch>
2021-11-10 21:04:00 +0100

    format

--- App.tsx
+++ App.tsx
@@ -1,51 +1,54 @@
-import { Button, Pressable, Text } from 'react-native';
-import React, { useState } from 'react';
-import styled, { css } from '@emotion/native'
+import {Button, Pressable, Text} from 'react-native';
+import React, {useState} from 'react';
+import styled, {css} from '@emotion/native';
 
-import { useStopwatch } from './src/useStopwatch';
+import {useStopwatch} from './src/useStopwatch';
 
 const Top = styled.View`
   height: 50%;
-  justify-content: center;  
+  justify-content: center;
   align-items: center;
-  background-color: #000;  
-`
+  background-color: #000;
+`;
 
 const Bottom = styled.View`
-height: 50%;
-justify-content: center;  
-background-color: #000; 
-`
+  height: 50%;
+  justify-content: center;
+  background-color: #000;
+`;
 
 const Timer = styled.Text`
   color: #fff;
-  fontSize: 84px;
+  font-size: 84px;
   font-family: 'lucida grande'; // TODO: How do I change to another system font?
-`
+`;
 
 const RoundButton = css`
   border-radius: 50px;
   background-color: green;
   boarder-width: 20px;
-  boarder-color: green;  
+  boarder-color: green;
   width: 100px;
   height: 100px;
-`
+`;
 
 const App = () => {
-  var [watch, setWatch] = useState("00:00:00");
-  var {start, reset} = useStopwatch(setWatch);
-   return <>
-     <Top>
-       <Timer>{watch}</Timer>
-     </Top>
-     <Bottom>
-       <Button title="Start" onPress={start}/>
-       <Text> --- </Text>
-       <Button title="Reset" onPress={reset}/>
-     </Bottom>
-   </>;
- };
+  var [watch, setWatch] = useState('00:00:00');
+  var {start, reset, running} = useStopwatch(setWatch);
+  return (
+    <>
+      <Top>
+        <Timer>{watch}</Timer>
+      </Top>
+      <Bottom>
+        {!running ? (
+          <Button title="Start" onPress={start} />
+        ) : (
+          <Button title="Reset" onPress={reset} />
+        )}
+      </Bottom>
+    </>
+  );
+};
 
- 
- export default App;
+export default App;
--- src/useStopwatch.ts
+++ src/useStopwatch.ts
@@ -1,49 +1,49 @@
-import { useCallback, useEffect, useRef, useState } from "react";
+import {useCallback, useEffect, useRef, useState} from 'react';
 
 const formatTime = (time: number): string => {
-    const hours = Math.floor(time / 60 / 60);
-    const minutes = Math.floor(time / 60) % 60;
-    const seconds = time % 60;
-    const leadingZero = (num: number) => num < 10 ? `0${num}` : num;
-    return `${leadingZero(hours)}:${leadingZero(minutes)}:${leadingZero(seconds)}`;
-}
+  const leadingZero = (num: number) => (num < 10 ? `0${num}` : num);
+  const hours = leadingZero(Math.floor(time / 60 / 60));
+  const minutes = leadingZero(Math.floor(time / 60) % 60);
+  const seconds = leadingZero(time % 60);
 
-export const useStopwatch = (callback: (a: string) => void) => {
-    const callbackRef = useRef(callback);
-    const intervalRef = useRef<number>();
-    
-    useEffect(() => {
-        callbackRef.current = callback;
-    }, [callback]);
-
-    const [time, setTime] = useState(0);
-    const [running, setRunning] = useState(true);
-
-    useEffect(() => {
-        callbackRef.current(formatTime(time));
-    }, [time]);            
-
-    const start = useCallback(() => {
-        setRunning(true);
-        intervalRef.current = <number><unknown>setInterval(() => {//TODO: Type!            
-            setTime(time => time + 1);}, 
-            1000); 
-    }, []);
-
-    const reset = useCallback(() => {
-        setTime(0);
-        setRunning(false);
-        if (intervalRef.current) {
-            clearInterval(intervalRef.current);
-        }        
-    }, []);
-
-    useEffect(() => {
-        start();
-        return reset;
-    }, [start, reset]);
-
-    return {start, reset, running};
+  return `${hours}:${minutes}:${seconds}`;
 };
 
+export const useStopwatch = (callback: (a: string) => void) => {
+  const callbackRef = useRef(callback);
+  const intervalRef = useRef<number>();
 
+  useEffect(() => {
+    callbackRef.current = callback;
+  }, [callback]);
+
+  const [time, setTime] = useState(0);
+  const [running, setRunning] = useState(true);
+
+  useEffect(() => {
+    callbackRef.current(formatTime(time));
+  }, [time]);
+
+  const start = useCallback(() => {
+    setRunning(true);
+    intervalRef.current = <number>(<unknown>setInterval(() => {
+      //TODO: Type!
+      setTime(time => time + 1);
+    }, 1000));
+  }, []);
+
+  const reset = useCallback(() => {
+    setTime(0);
+    setRunning(false);
+    if (intervalRef.current) {
+      clearInterval(intervalRef.current);
+    }
+  }, []);
+
+  useEffect(() => {
+    start();
+    return reset;
+  }, [start, reset]);
+
+  return {start, reset, running};
+};
